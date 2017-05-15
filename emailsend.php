6082a567dfbdb9f88efc66575f2a16e7af207c41
DESKTOP-8BK3PKQ\Daniel <danbed1990@gmail.com>
2017-05-15 22:06:06 +0200

    add description, change backgorund

--- emailsend.php
+++ emailsend.php
@@ -3,7 +3,6 @@
 require 'phpmailer/PHPMailerAutoload.php';
 $name = $_POST["name"];
 $lastname = $_POST["lastname"];
-$phone = $_POST["phone"];
 $email = $_POST["email"];
 $message = $_POST["message"];
 
@@ -13,27 +12,24 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'crm@paretti.pl';                 // SMTP username
-$mail->Password = '';                          // SMTP password
+$mail->Username = 'danielbednarczuk90@gmail.com';                 // SMTP username
+$mail->Password = 'matematyka';                          // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->setFrom('crm@paretti.pl', 'Paretti szkolenia');
-$mail->addAddress('daniel.bednarczuk@paretti.pl');     // Add a recipient
+$mail->setFrom('danielbednarczuk90@gmail.com', 'Formularz ');
+$mail->addAddress('info@danielbednarczuk.pl');     // Add a recipient
 
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Zgłoszenie na kurs VCA';
-$mail->Body    = "Nowe zgłoszenie na kurs VCA<br>
-                <b>imie :</b>".$name."<br>
+$mail->Subject = 'Wiadomosc ze strony';
+$mail->Body    = "<b>imie :</b>".$name."<br>
                 <b>nazwisko :</b>".$lastname."<br>
-                <b>numer telefonu :</b>".$phone."<br>
                 <b>email :</b>".$email."<br>
                 <b>wiadomosc</b>".$message;
 
 if(!$mail->send()) {
-    header('Location:index.html');
-
+   echo $mail->ErrorInfo;
 } else {
     header('Location:index.html');
 }
